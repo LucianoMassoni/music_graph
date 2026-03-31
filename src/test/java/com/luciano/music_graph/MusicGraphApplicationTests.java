fe97c3da875e0bd6569c440b6c45b26e294db844
Luciano Massoni <massoni.luciano7@gmail.com>
2026-03-31 18:01:37 -0300

    feat(config): add integration test base and test profile configuration

--- src/test/java/com/luciano/music_graph/MusicGraphApplicationTests.java
+++ src/test/java/com/luciano/music_graph/MusicGraphApplicationTests.java
@@ -1,10 +1,28 @@
 package com.luciano.music_graph;
 
+import com.luciano.music_graph.client.LastFmClient;
+import com.luciano.music_graph.service.JwtService;
+import com.luciano.music_graph.service.RefreshTokenService;
+import com.luciano.music_graph.utils.CookieUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-class MusicGraphApplicationTests {
+class MusicGraphApplicationTests extends BaseIntegrationTest {
+
+	@MockitoBean
+	private LastFmClient lastFmClient;
+
+	@MockitoBean
+	private JwtService jwtService;
+
+	@MockitoBean
+	private RefreshTokenService refreshTokenService;
+
+	@MockitoBean
+	private CookieUtils cookieUtils;
 
 	@Test
 	void contextLoads() {
