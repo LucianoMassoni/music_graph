6fd1a2483ba2fea6b0c06f0e817980c1d389b2b0
Luciano Massoni <massoni.luciano7@gmail.com>
2026-06-02 17:17:07 -0300

    refactor(graph): update mappers and repositories to use UserArtist relations.

--- src/main/java/com/luciano/music_graph/mapper/GraphMapper.java
+++ src/main/java/com/luciano/music_graph/mapper/GraphMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface GraphMapper {
 
-    ShortArtistInfoDto toShortArtistInfoDto(Artist artist);
+    ShortArtistInfoDto toShortArtistInfoDto(Artist artist, boolean isFollowed);
 }
